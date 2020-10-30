725514615e86ad9c44a902f51f42bccec717d1de
Talagozis <talagozis8@gmail.com>
2020-10-30 13:40:45 +0200

    Upgrade Phoenix.DataHandle package.

--- Phoenix.Api/Models/Api/AspNetUserApi.cs
+++ Phoenix.Api/Models/Api/AspNetUserApi.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Phoenix.DataHandle.Main;
 using Phoenix.DataHandle.Main.Entities;
 using Phoenix.DataHandle.Main.Relationships;
 
@@ -15,8 +16,8 @@ namespace Phoenix.Api.Models.Api
         public string PhoneNumber { get; set; }
         public bool PhoneNumberConfirmed { get; set; }
         public string FacebookId { get; set; }
-        public DateTime RegisteredAt { get; set; }
-        public int CreatedApplicationType { get; set; }
+        public DateTimeOffset RegisteredAt { get; set; }
+        public ApplicationType CreatedApplicationType { get; set; }
 
         public UserApi User { get; set; }
         IUser IAspNetUsers.User => this.User;
